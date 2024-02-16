d68416846d4a20c47dc71dc7dacd073d0bbcef4f
pyc <1115040131@qq.com>
2024-02-16 23:32:02 +0800

    添加grpc支持，优化bazel copts

---
+++ network/example/grpc_server.cpp
@@ -0,0 +1,11 @@
+#include "network/proto/greeter.pb.h"
+
+// #include <grpc/
+
+namespace network {
+
+// class GreeterServiceImpl fianl : public
+
+}  // namespace network
+
+int main() { return 0; }
