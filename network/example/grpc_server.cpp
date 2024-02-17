9db6a8963bb42b8973e9419ca6e451524348cb5f
pyc <1115040131@qq.com>
2024-02-17 14:03:04 +0800

    添加grpc服务器

--- network/example/grpc_server.cpp
+++ network/example/grpc_server.cpp
@@ -1,11 +1,34 @@
-#include "network/proto/greeter.pb.h"
+#include <fmt/core.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
 
-// #include <grpc/
+#include "network/proto/greeter.grpc.pb.h"
 
 namespace network {
 
-// class GreeterServiceImpl fianl : public
+class GreeterServiceImpl final : public Greeter::Service {
+    grpc::Status SayHello([[maybe_unused]] grpc::ServerContext* context, const network::HelloRequest* request,
+                          network::HelloReply* response) override {
+        response->set_message(fmt::format("Greeter server receive: \"{}\"", request->message()));
+        return grpc::Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("127.0.0.1:50051");
+    GreeterServiceImpl service;
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    fmt::println("Server listening on {}", server_address);
+    server->Wait();
+}
 
 }  // namespace network
 
-int main() { return 0; }
+int main() {
+    network::RunServer();
+    return 0;
+}
