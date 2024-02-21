d04db27fb9ad9487dc7306837fc1eb2fbc075d89
pyc <1115040131@qq.com>
2024-02-22 01:03:00 +0800

    互斥和死锁 test

--- common/include/common/utils.h
+++ common/include/common/utils.h
@@ -5,13 +5,16 @@
 
 namespace pyc {
 
-// 转换枚举到其底层类型的通用函数
+/// @brief 转换枚举到其底层类型的通用函数
 template <typename E>
 constexpr auto ToUnderlying(E e) -> typename std::underlying_type<E>::type {
     return static_cast<typename std::underlying_type<E>::type>(e);
 }
 
-// 从带有作用域的函数名中删除命名空间和类名
+/// @brief 从带有作用域的函数名中删除命名空间和类名
 std::string_view GetSimpleName(std::string_view functionName);
+
+/// @brief 将线程 ID 转换为 string
+std::string GetThreadId();
 
 }  // namespace pyc
