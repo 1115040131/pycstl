65829c8bc775db8d0f16d0511e1a79495f9eca85
pyc <1115040131@qq.com>
2024-06-09 15:50:50 +0800

    组件模式

---
+++ design_pattern/component_pattern.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace pyc {
+
+struct vec3 {
+    float x, y, z;
+};
+
+struct Component {
+    virtual void update(GameObject* go) = 0;
+
+    // 如果 Component 全部都是使用 make_shared 创建的, 即使 Component 没有虚析构函数, 也可以安全地销毁
+    // 因为 shared_ptr 会对 deleter 进行类型擦除. make_shared 会在构造时记住子类的 deleter
+    // make_unique 和 new 创建的就会泄露
+    virtual ~Component() = default;
+};
+
+struct GameObject {
+    std::vector<std::unique_ptr<Component>> components;
+
+    void add(std::unique_ptr<Component> component) { components.push_back(std::move(component)); }
+
+    void update() {
+        for (auto& component : components) {
+            component->update(this);
+        }
+    }
+
+    void kill() {}
+};
+
+struct Movable : Component {
+    vec3 position;
+    vec3 velocity;
+
+    void update(GameObject* go) override {
+        position.x += velocity.x;
+        position.y += velocity.y;
+        position.z += velocity.z;
+    }
+};
+
+struct LivingBeging : Component {
+    int ageLeft;
+
+    void update(GameObject* go) override {
+        if (ageLeft < 0) {
+            go->kill();
+        } else {
+            ageLeft -= 1;
+        }
+    }
+};
+
+struct PlayerController : Component {
+    void update(GameObject* go) override {
+        // 读取输入，更新玩家的速度
+    }
+};
+
+struct PlayerAppearence : Component {
+    void update(GameObject* go) override {
+        // 渲染玩家
+    }
+};
+
+// 组件作为普通对象, 由 GameObject 构造函数创建
+struct Player : GameObject {
+    Movable* movable;
+    LivingBeging* living;
+    PlayerController* controller;
+    PlayerAppearence* appearence;
+
+    Player() {
+        movable = new Movable();
+        living = new LivingBeging();
+        controller = new PlayerController();
+        appearence = new PlayerAppearence();
+
+        add(std::unique_ptr<Movable>(movable));
+        add(std::unique_ptr<LivingBeging>(living));
+        add(std::unique_ptr<PlayerController>(controller));
+        add(std::unique_ptr<PlayerAppearence>(appearence));
+    }
+};
+
+// 一个普通函数创建具有 Player 所需所有组件的 GameObject 对象
+std::unique_ptr<GameObject> makePlayer() {
+    auto go = std::make_unique<GameObject>();
+    go->add(std::make_unique<Movable>());
+    go->add(std::make_unique<LivingBeging>());
+    go->add(std::make_unique<PlayerController>());
+    go->add(std::make_unique<PlayerAppearence>());
+    return go;
+}
+
+// 组件之间的通信
+
+}  // namespace pyc
