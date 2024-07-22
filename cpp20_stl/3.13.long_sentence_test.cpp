c78dd500832f71a400d8dfe57ed8d41f911688a9
pyc <1115040131@qq.com>
2024-07-22 11:43:03 +0800

    3.13.找出含有相应长句的 vector

---
+++ cpp20_stl/3.13.long_sentence_test.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <fstream>
+#include <string_view>
+#include <vector>
+
+#include <fmt/core.h>
+#include <gtest/gtest.h>
+
+#include "common/cin_redirect.h"
+
+namespace pyc {
+
+/// @brief 是否到句子末尾
+bool is_eos(std::string_view str) {
+    constexpr std::string_view eos{".!?"};
+    for (auto c : str) {
+        if (eos.find(c) != std::string_view::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void long_sentence() {
+    std::vector<std::vector<std::string>> sentences;
+    sentences.emplace_back();
+    for (std::string s{}; std::cin >> s;) {
+        sentences.back().emplace_back(s);
+        if (is_eos(s)) {
+            sentences.emplace_back();
+        }
+    }
+    if (sentences.back().empty()) {
+        sentences.pop_back();
+    }
+
+    std::ranges::sort(sentences, [](const std::vector<std::string>& lhs, const std::vector<std::string>& rhs) {
+        return lhs.size() > rhs.size();
+    });
+
+    constexpr int kLimit = 10;
+    for (const auto& sentence : sentences) {
+        size_t size = sentence.size();
+        size_t limit{kLimit};
+        fmt::print("{}: ", size);
+        for (const auto& word : sentence) {
+            fmt::print("{} ", word);
+            if (--limit == 0) {
+                if (size > kLimit) {
+                    fmt::print("...");
+                }
+                break;
+            }
+        }
+        fmt::print("\n");
+    }
+}
+
+TEST(LongSentenceTest, LongSentenceTest) {
+    std::ifstream input_file("cpp20_stl/resource/sentences.txt");
+    CinRedirect cin_redirect(input_file);
+    long_sentence();
+}
+
+}  // namespace pyc
