c81ff6a663b4e1e54c37e0459457ea3da219ef3c
pyc <1115040131@qq.com>
2024-09-06 10:21:54 +0800

    优化 gate_server_test

--- chat/server/gate_server/test/gate_server_test.py
+++ chat/server/gate_server/test/gate_server_test.py
@@ -1,4 +1,5 @@
 # gate_server_test.py
+import configparser
 import redis
 import requests
 import subprocess
@@ -29,6 +30,22 @@ class GateServerTest(unittest.TestCase):
     def setUpClass(cls):
         # 启动服务器
         cls.server_process = subprocess.Popen(['chat/server/gate_server/gate_server'])
+
+        # 初始化配置解析器
+        cls.config = configparser.ConfigParser()
+        # 读取 ini 文件
+        cls.config.read('chat/server/common/config/config.ini')
+
+        gate_server_port = cls.config['GateServer']['Port']
+        cls.gate_server_url = f'http://localhost:{gate_server_port}'
+
+        # 创建 redis 连接
+        cls.redis = redis.Redis(
+            host=cls.config['Redis']['Host'],
+            port=cls.config['Redis']['Port'],
+            password=cls.config['Redis']['Password'],
+        )
+
         time.sleep(1)  # 给服务器足够时间启动
 
     @classmethod
@@ -39,32 +56,32 @@ class GateServerTest(unittest.TestCase):
 
     def test_not_found(self):
         # 不存在的 url
-        response = requests.get('http://localhost:8080/not_found')
+        response = requests.get(f'{self.gate_server_url}/not_found')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.headers['Content-Type'], 'text/plain')
         self.assertEqual(response.text, "Url not found\r\n")
 
-        response = requests.post('http://localhost:8080/not_found')
+        response = requests.post(f'{self.gate_server_url}/not_found')
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.headers['Content-Type'], 'text/plain')
         self.assertEqual(response.text, "Url not found\r\n")
 
     def test_get_test(self):
-        response = requests.get('http://localhost:8080/get_test')
+        response = requests.get(f'{self.gate_server_url}/get_test')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "receive get_test req\n")
 
         # 带参数的 get 请求
-        response = requests.get('http://localhost:8080/get_test?')
+        response = requests.get(f'{self.gate_server_url}/get_test?')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "receive get_test req\n")
 
-        response = requests.get('http://localhost:8080/get_test?a=1')
+        response = requests.get(f'{self.gate_server_url}/get_test?a=1')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "receive get_test req\na: 1\n")
 
         response = requests.get(
-            'http://localhost:8080/get_test?a=1&=2&c=&d=+4&%E4%BB%8A%E5%A4%A9=%E6%98%9F%E6%9C%9F%E4%B8%80')
+            f'{self.gate_server_url}/get_test?a=1&=2&c=&d=+4&%E4%BB%8A%E5%A4%A9=%E6%98%9F%E6%9C%9F%E4%B8%80')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "receive get_test req\n今天: 星期一\nd:  4\na: 1\n")
 
@@ -73,20 +90,20 @@ class GateServerTest(unittest.TestCase):
         incorrect_json = "{'this is not': 'a valid json'}"  # 错误：属性名没有使用双引号
         # 设置请求头来指明发送的是JSON数据
         headers = {'Content-Type': 'application/json'}
-        response = requests.post('http://localhost:8080/get_verifycode', data=incorrect_json, headers=headers)
+        response = requests.post(f'{self.gate_server_url}/get_verifycode', data=incorrect_json, headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
         self.assertEqual(json_response['error'], ErrorCode.kJsonError.value)
 
         # email key 不存在
-        response = requests.post('http://localhost:8080/get_verifycode', json='')
+        response = requests.post(f'{self.gate_server_url}/get_verifycode', json='')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
         self.assertEqual(json_response['error'], ErrorCode.kJsonError.value)
 
-        response = requests.post('http://localhost:8080/get_verifycode', json={'email': 'test_email@pyc.com'})
+        response = requests.post(f'{self.gate_server_url}/get_verifycode', json={'email': 'test_email@pyc.com'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
@@ -98,7 +115,7 @@ class GateServerTest(unittest.TestCase):
         incorrect_json = "{'this is not': 'a valid json'}"  # 错误：属性名没有使用双引号
         # 设置请求头来指明发送的是JSON数据
         headers = {'Content-Type': 'application/json'}
-        response = requests.post('http://localhost:8080/user_register', data=incorrect_json, headers=headers)
+        response = requests.post(f'{self.gate_server_url}/user_register', data=incorrect_json, headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
@@ -108,24 +125,17 @@ class GateServerTest(unittest.TestCase):
         email = 'pyc@stl.test'
 
         # 确认密码不一致
-        response = requests.post('http://localhost:8080/user_register', json={'email': email,
-                                                                              'password': '123',
-                                                                              "confirm": '456'})
+        response = requests.post(f'{self.gate_server_url}/user_register', json={'email': email,
+                                                                                'password': '123',
+                                                                                "confirm": '456'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
         self.assertEqual(json_response['error'], ErrorCode.kPasswordError.value)
 
-        # 创建 redis 连接
-        r = redis.Redis(
-            host='localhost',
-            port=6380,
-            password='123456'
-        )
-
         # 验证码不存在或超时
-        r.delete("code_" + email)
-        response = requests.post('http://localhost:8080/user_register', json={'email': email})
+        self.redis.delete("code_" + email)
+        response = requests.post(f'{self.gate_server_url}/user_register', json={'email': email})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
@@ -133,18 +143,16 @@ class GateServerTest(unittest.TestCase):
 
         # 验证码错误
         verify_code = "qwer"
-        r.set("code_" + email, verify_code)
-        response = requests.post('http://localhost:8080/user_register', json={'email': email})
+        self.redis.set("code_" + email, verify_code)
+        response = requests.post(f'{self.gate_server_url}/user_register', json={'email': email})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
         self.assertEqual(json_response['error'], ErrorCode.kVerifyCodeError.value)
 
         # 注册成功
-        verify_code = "qwer"
-        r.set("code_" + email, verify_code)
-        response = requests.post('http://localhost:8080/user_register', json={'email': email,
-                                                                              'verify_code': verify_code})
+        response = requests.post(f'{self.gate_server_url}/user_register', json={'email': email,
+                                                                                'verify_code': verify_code})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'text/json')
         json_response = response.json()
