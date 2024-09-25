1a272cfb7e8cfd91ab9d092c323cf383bf192f6b
pyc <1115040131@qq.com>
2024-09-26 01:29:01 +0800

    verify_grpc_client 复用 connection_pool

--- chat/server/gate_server/include/gate_server/verify_grpc_client.h
+++ chat/server/gate_server/include/gate_server/verify_grpc_client.h
@@ -1,50 +1,26 @@
 #pragma once
 
-#include <atomic>
-#include <condition_variable>
-#include <memory>
-#include <mutex>
-#include <queue>
-#include <string_view>
-
-#include <grpcpp/create_channel.h>
-
 #include "chat/server/proto/verify.grpc.pb.h"
 #include "common/singleton.h"
 
 namespace pyc {
 namespace chat {
 
-class RpcConnectionPool {
-public:
-    RpcConnectionPool(std::size_t size, std::string_view host, std::string_view port);
-
-    ~RpcConnectionPool();
-
-    void Close();
-
-    std::unique_ptr<VerifyService::Stub> GetConnection();
-
-    void ReturnConnection(std::unique_ptr<VerifyService::Stub> connection);
-
-private:
-    std::atomic<bool> stop_{false};
-    std::queue<std::unique_ptr<VerifyService::Stub>> connections_;
-    std::condition_variable cond_;
-    std::mutex mutex_;
-};
+class VerifyConnectionPool;
 
 class VerifyGrpcClient : public Singleton<VerifyGrpcClient> {
     friend class Singleton<VerifyGrpcClient>;
 
-public:
-    VerifyResponse GetVerifyCode(std::string_view email);
-
 private:
     VerifyGrpcClient();
 
+public:
+    ~VerifyGrpcClient();
+
+    VerifyResponse GetVerifyCode(std::string_view email);
+
 private:
-    std::unique_ptr<RpcConnectionPool> pool_;
+    std::unique_ptr<VerifyConnectionPool> pool_;
 };
 
 }  // namespace chat
--- chat/server/gate_server/src/verify_grpc_client.cpp
+++ chat/server/gate_server/src/verify_grpc_client.cpp
@@ -1,76 +1,51 @@
 #include "gate_server/verify_grpc_client.h"
 
+#include <grpcpp/create_channel.h>
+
 #include "chat/server/common/config_mgr.h"
+#include "chat/server/common/defer.h"
+#include "common/connection_pool.h"
 #include "gate_server/define.h"
 
 namespace pyc {
 namespace chat {
 
-RpcConnectionPool::RpcConnectionPool(std::size_t size, std::string_view host, std::string_view port) {
-    auto target = fmt::format("{}:{}", host, port);
-    for (std::size_t i = 0; i < size; ++i) {
-        connections_.push(VerifyService::NewStub(grpc::CreateChannel(target, grpc::InsecureChannelCredentials())));
+class VerifyConnectionPool : public ConnectionPool<std::unique_ptr<VerifyService::Stub>> {
+public:
+    VerifyConnectionPool(std::string_view host, std::string_view port, size_t size) {
+        auto target = fmt::format("{}:{}", host, port);
+        for (std::size_t i = 0; i < size; ++i) {
+            connections_.push(
+                VerifyService::NewStub(grpc::CreateChannel(target, grpc::InsecureChannelCredentials())));
+        }
     }
-}
-
-RpcConnectionPool::~RpcConnectionPool() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    Close();
-    while (!connections_.empty()) {
-        connections_.pop();
-    }
-}
-
-void RpcConnectionPool::Close() {
-    stop_ = true;
-    cond_.notify_all();
-}
-
-std::unique_ptr<VerifyService::Stub> RpcConnectionPool::GetConnection() {
-    std::unique_lock<std::mutex> lock(mutex_);
-    cond_.wait(lock, [this] { return !connections_.empty() || stop_; });
-
-    if (stop_) {
-        return nullptr;
-    }
-
-    auto connection = std::move(connections_.front());
-    connections_.pop();
-    return connection;
-}
-
-void RpcConnectionPool::ReturnConnection(std::unique_ptr<VerifyService::Stub> connection) {
-    std::lock_guard<std::mutex> lock(mutex_);
-
-    if (stop_) {
-        return;
-    }
-
-    connections_.push(std::move(connection));
-    cond_.notify_one();
-}
+};
 
 VerifyGrpcClient::VerifyGrpcClient() {
     GET_CONFIG(host, "VerifyServer", "Host");
     GET_CONFIG(port, "VerifyServer", "Port");
-    pool_ = std::make_unique<RpcConnectionPool>(5, host, port);
+    pool_ = std::make_unique<VerifyConnectionPool>(host, port, 5);
 }
 
+VerifyGrpcClient::~VerifyGrpcClient() { pool_->Close(); }
+
 VerifyResponse VerifyGrpcClient::GetVerifyCode(std::string_view email) {
+    auto connection = pool_->GetConnection();
+    if (!connection) {
+        return {};
+    }
+    Defer defer([this, &connection]() { this->pool_->ReturnConnection(std::move(*connection)); });
+
     grpc::ClientContext context;
     VerifyRequest request;
     request.set_email(email);
     VerifyResponse response;
 
-    auto stub = pool_->GetConnection();
-
-    auto status = stub->GetVerifyCode(&context, request, &response);
+    auto status = connection.value()->GetVerifyCode(&context, request, &response);
     if (status.ok()) {
-        pool_->ReturnConnection(std::move(stub));
         return response;
     }
 
-    pool_->ReturnConnection(std::move(stub));
     response.set_error(static_cast<int32_t>(ErrorCode::kRpcFailed));
     return response;
 }
