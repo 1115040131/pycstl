f0e1ebe3c23f0ae8decc38e6edf0afbac96b5f64
pyc <1115040131@qq.com>
2024-11-14 23:35:28 +0800

    优化redis_mgr 头文件

--- chat/server/common/src/redis_mgr.cpp
+++ chat/server/common/src/redis_mgr.cpp
@@ -1,81 +1,10 @@
 #include "chat/server/common/redis_mgr.h"
 
 #include "chat/server/common/config_mgr.h"
-#include "logger/logger.h"
+#include "chat/server/common/redis_connection_pool.h"
 
 namespace pyc {
 namespace chat {
-
-inline Logger _g_redis_mgr_logger("RedisMgr");
-
-inline bool RedisReplyOk(redisReply* reply) {
-    return reply && reply->type == REDIS_REPLY_STATUS && ::strcasecmp(reply->str, "OK") == 0;
-}
-
-inline void redisContextDeleter(redisContext* r) { redisFree(r); }
-
-RedisConnectionPool::RedisConnectionPool(size_t size, std::string_view host, int port, std::string_view password) {
-    for (size_t i = 0; i < size; i++) {
-        auto context = ConnectionImpl(redisConnect(host.data(), port), redisContextDeleter);
-
-        // 连接失败
-        if (!context || context->err) {
-            _g_redis_mgr_logger.error("Connect error: {}", context->errstr);
-            continue;
-        }
-
-        if (!password.empty()) {
-            auto command = fmt::format("AUTH {}", password);
-            auto guard = RedisReplyGuard(context.get(), command);
-
-            // 认证失败
-            if (!RedisReplyOk(guard.reply_)) {
-                _g_redis_mgr_logger.error("[{}] failed", command);
-                continue;
-            }
-            _g_redis_mgr_logger.info("[{}] success", command);
-        }
-
-        connections_.push(std::move(context));
-    }
-}
-
-RedisConnectionPool::~RedisConnectionPool() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    Close();
-    while (!connections_.empty()) {
-        connections_.pop();
-    }
-}
-
-void RedisConnectionPool::Close() {
-    stop_ = true;
-    cond_.notify_all();
-}
-
-std::optional<RedisConnectionPool::Connection> RedisConnectionPool::GetConnection() {
-    std::unique_lock<std::mutex> lock(mutex_);
-    cond_.wait(lock, [this] { return !connections_.empty() || stop_; });
-
-    if (stop_) {
-        return std::nullopt;
-    }
-
-    Connection connection(std::move(connections_.front()), this);
-    connections_.pop();
-    return connection;
-}
-
-void RedisConnectionPool::ReturnConnection(ConnectionImpl connection) {
-    std::lock_guard<std::mutex> lock(mutex_);
-
-    if (stop_) {
-        return;
-    }
-
-    connections_.push(std::move(connection));
-    cond_.notify_one();
-}
 
 RedisMgr::RedisMgr() {
     GET_CONFIG(host, "Redis", "Host");
@@ -84,7 +13,7 @@ RedisMgr::RedisMgr() {
     pool_ = std::make_unique<RedisConnectionPool>(5, host, port, password);
 }
 
-void RedisMgr::Close() { pool_->Close(); }
+RedisMgr::~RedisMgr() { pool_->Close(); }
 
 std::optional<std::string> RedisMgr::Get(std::string_view key) {
     auto connection = pool_->GetConnection();
